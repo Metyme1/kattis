f8c1ff88315c1efbe2af977f18c544a769b28803
Metyme1 <millionmety@gmail.com>
2023-08-01 14:42:56 +0300

    initial commit

---
+++ which is greater.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long int a, b;
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << 1 << "\n";
+    }
+    else
+    {
+        cout << 0 << "\n";
+    }
+}
